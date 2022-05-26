4f8f3534c7543e0be7650b8cc60ed217fb05fb78
sokolw <sokolw21@gmail.com>
2022-05-26 13:22:33 +0300

    fix: some fix

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -21,14 +21,39 @@ async function copyDir(innerTargetFolder = null, innerDistFolder = null) {
       if (distFiles !== null){
         if (!distFiles.includes(file.name)) {
           fsPromises.copyFile(path.join(isRecursive(innerTargetFolder) ?? targetFolder, file.name), path.join(isRecursive(innerDistFolder) ?? distFolder, file.name));
+          distFiles.splice(distFiles.indexOf(file.name), 1);
+        } else {
+          distFiles.splice(distFiles.indexOf(file.name), 1);
         }
       } else {
         fsPromises.copyFile(path.join(isRecursive(innerTargetFolder) ?? targetFolder, file.name), path.join(isRecursive(innerDistFolder) ?? distFolder, file.name));
       }
     } else if (file.isDirectory()){
+      if (distFiles !== null) {
+        distFiles.splice(distFiles.indexOf(file.name), 1);
+      }
       copyDir(path.join(isRecursive(innerTargetFolder) ?? targetFolder, file.name), path.join(isRecursive(innerDistFolder) ?? distFolder, file.name));
     }
   });
+  // fix inject
+  if (distFiles !== null) {
+    let distFilesWithTypes = null;
+    try {
+      await fsPromises.access(isRecursive(innerDistFolder) ?? distFolder, fs.constants.R_OK);
+      distFilesWithTypes = await fsPromises.readdir(isRecursive(innerDistFolder) ?? distFolder, {withFileTypes:true});
+    } catch {
+      await fsPromises.mkdir(isRecursive(innerDistFolder) ?? distFolder, {recursive:true});
+    }
+    distFilesWithTypes.forEach((file) => {
+      if (distFiles.includes(file.name)){
+        if(file.isFile()) {
+          fs.unlink(path.join(isRecursive(innerDistFolder) ?? distFolder, file.name), (err) => {err;});
+        } else {
+          fs.rm(path.join(isRecursive(innerDistFolder) ?? distFolder, file.name), {recursive:true}, (err) => {err;});
+        }
+      }
+    });
+  }
 }
 
 function isRecursive (folder) {
